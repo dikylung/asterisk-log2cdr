63da0bc409bd4cb40aefdcdec50e32d683e03e98
Michel Alexandre Salim <michel@cergis.net.id>
2014-04-08 11:54:57 +0700

    Grab an earlier line in the logs to get caller ID, channel and source extension

--- log2cdr/__init__.py
+++ log2cdr/__init__.py
@@ -4,13 +4,19 @@ import csv
 from datetime import datetime
 import sys
 
+DEBUG = False
+
 class Call(object):
-    def __init__(self, id, caller, start_time, receiver=None, end_time=None):
+    def __init__(self, id, start_time, clid, src, channel, accountcode=None, dst=None, end_time=None, answered=False):
         self.id = id
-        self.caller = caller
         self.start_time = start_time
-        self.receiver = receiver
+        self.clid = clid
+        self.src = src
+        self.channel = channel
+        self.accountcode = accountcode
+        self.dst = dst
         self.end_time = end_time
+        self.answered = answered
 
     def duration(self):
         return self.end_time - self.start_time
@@ -50,56 +56,107 @@ class Log2CDR(object):
                     try:
                         log_entry = LogEntry(start_time.year, line)
                     except ValueError:
-                        print("Cannot parse line: " + line)
+                        #print("Cannot parse line: " + line)
                         continue
                     if log_entry.tstamp < start_time:
                         continue
                     if log_entry.tstamp > end_time:
                         break
                     if log_entry.src == 'pbx.c':
-                        i = log_entry.msg.find('Set(CDR(accountcode)=')
+                        # src, channel and caller ID
+                        i = log_entry.msg.find('CALLERID(all)="')
                         if i != -1:
-                            #print('Found call start: id={0} time={1}'.format(log_entry.event_id, log_entry.tstamp))
                             if log_entry.event_id in self.calls:
-                                sys.stderr.write('Currently cannot handle reused event IDs: {0}'.format(log_entry.event_id))
+                                sys.stderr.write('Currently cannot handle reused event IDs: {0}\n'.format(log_entry.event_id))
                                 sys.stderr.flush()
                                 self.err_count += 1
                             else:
-                                ph_start_i = i + len('Set(CDR(accountcode)=')
-                                ph_end_i = log_entry.msg.find(')', ph_start_i)
-                                ph_caller = log_entry.msg[ph_start_i:ph_end_i]
+                                clid_start_i = i + len('CALLERID(all)=')
+                                clid_end_i = log_entry.msg.find('")', clid_start_i)
+                                clid = log_entry.msg[clid_start_i:clid_end_i]
+
+                                chn_start_i = log_entry.msg.find('Set("SIP/') + len('Set("')
+                                chn_end_i = log_entry.msg.find('", "', chn_start_i)
+                                chn = log_entry.msg[chn_start_i:chn_end_i]
+
+                                # source extension
+                                src = chn.split('/')[-1].split('-')[0]
+
                                 self.calls[log_entry.event_id] = Call(
                                     log_entry.event_id,
-                                    ph_caller,
-                                    log_entry.tstamp)
-                        elif log_entry.msg.find('-- Goto (macro-hangupcall') != -1:
-                            #print('Found call end: id={0} time={1}'.format(log_entry.event_id, log_entry.tstamp))
+                                    log_entry.tstamp,
+                                    clid,
+                                    src,
+                                    chn)
+
+
+                        # accountcode
+                        else:
+                            i = log_entry.msg.find('Set(CDR(accountcode)=')
+                            if i != -1:
+                                #print('Found call start: id={0} time={1}'.format(log_entry.event_id, log_entry.tstamp))
+                                try:
+                                    call = self.calls[log_entry.event_id]
+                                    ph_start_i = i + len('Set(CDR(accountcode)=')
+                                    ph_end_i = log_entry.msg.find(')', ph_start_i)
+                                    accountcode = log_entry.msg[ph_start_i:ph_end_i]
+                                    call.accountcode = accountcode
+                                except KeyError:
+                                    if DEBUG:
+                                        sys.stderr.write('Processing accountcode before call start:\n{0}\n'.format(line))
+                                        sys.stderr.flush()
+                                    self.err_count += 1
+                            elif log_entry.msg.find('-- Goto (macro-hangupcall') != -1:
+                                #print('Found call end: id={0} time={1}'.format(log_entry.event_id, log_entry.tstamp))
+                                try:
+                                    call = self.calls[log_entry.event_id]
+                                    if call.answered:
+                                        call.end_time = log_entry.tstamp
+                                        csvwriter.writerow([call.start_time,
+                                                            call.clid,
+                                                            call.src,
+                                                            call.dst,
+                                                            call.channel,
+                                                            call.duration()
+                                        ])
+                                        self.ok_count += 1
+                                    del self.calls[log_entry.event_id]
+                                except KeyError:
+                                    if DEBUG:
+                                        sys.stderr.write('Processing end before call start:\n{0}\n'.format(line))
+                                        sys.stderr.flush()
+                                    self.err_count += 1
+
+                    elif log_entry.src == 'app_dial.c':
+                        # dst
+                        if log_entry.msg.find('-- Called SIP/') != -1:
+                            #print('Found receiver: id={0}'.format(log_entry.event_id) + log_entry.msg)
+                            splits = log_entry.msg.split('/')
+                            if len(splits) != 3:
+                                # not an outgoing call
+                                continue
+                            ph_dst = log_entry.msg.split('/')[-1][:-1]
                             try:
                                 call = self.calls[log_entry.event_id]
-                                call.end_time = log_entry.tstamp
-                                csvwriter.writerow([call.start_time,
-                                                    call.caller, # should be caller id
-                                                    call.caller, # src
-                                                    call.receiver, # dst
-                                                    call.duration()
-                                ])
-                                self.ok_count += 1
-                                del self.calls[log_entry.event_id]
+                                call.dst = ph_dst
                             except KeyError:
-                                sys.stderr.write('Processing end before call start:\n{0}\n'.format(line))
-                                sys.stderr.flush()
+                                if DEBUG:
+                                    sys.stderr.write('Processing receiver before call start:\n{0}\n'.format(line))
+                                    sys.stderr.flush()
+                                self.err_count += 1
+                        # call answered
+                        elif log_entry.msg.find(' answered SIP/') != -1:
+                            try:
+                                call = self.calls[log_entry.event_id]
+                                # override start time, we only bill from the time call is answered
+                                call.start_time = log_entry.tstamp
+                                call.answered = True
+                            except KeyError:
+                                if DEBUG:
+                                    sys.stderr.write('Processing answered status before call start:\n{0}\n'.format(line))
+                                    sys.stderr.flush()
                                 self.err_count += 1
 
-                    elif log_entry.src == 'app_dial.c' and log_entry.msg.find('-- Called SIP/') != -1:
-                        #print('Found receiver: id={0}'.format(log_entry.event_id) + log_entry.msg)
-                        ph_rcvr = log_entry.msg.split('/')[-1][:-1]
-                        try:
-                            call = self.calls[log_entry.event_id]
-                            call.receiver = ph_rcvr
-                        except KeyError:
-                            sys.stderr.write('Processing receiver before call start:\n{0}\n'.format(line))
-                            sys.stderr.flush()
-                            self.err_count += 1
 #                    if log_entry.msg.find('record-enable,,') != -1:
 #                        pass
                 print('Processed: {0} calls'.format(self.ok_count))
